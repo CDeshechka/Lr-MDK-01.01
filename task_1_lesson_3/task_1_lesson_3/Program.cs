0fa0aad8cbca4bada556fb04f095fc210936f7bf
CDeshechka <deniscvetkove@gmail.com>
2025-09-25 18:55:02 +0300

    Вводим содержание массива

---
+++ task_1_lesson_3/task_1_lesson_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1_lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводим содержание массива
+            Console.Write("Введите числа, содержащиеся в массиве через пробел: ");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }
+}
