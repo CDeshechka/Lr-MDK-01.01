ae3341abac2216a9eae7718a90d2d9007c4ecd19
CDeshechka <deniscvetkove@gmail.com>
2025-09-25 19:04:23 +0300

    Создаем массив

---
+++ task_2_lesson_3/task_2_lesson_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем массив
+            int[] numbers = new int[4000];
+            Random rnd = new Random();
+        }
+    }
+}
