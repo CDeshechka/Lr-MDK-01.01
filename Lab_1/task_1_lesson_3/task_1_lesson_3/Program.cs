c02a617e00992842625b0036adbd70849ca19c74
CDeshechka <deniscvetkove@gmail.com>
2025-10-16 21:00:44 +0300

    Добавил свои задания в папку Lab_1

---
+++ Lab_1/task_1_lesson_3/task_1_lesson_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1_lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вводим содержание массива
+            Console.Write("Введите числа, содержащиеся в массиве через пробел: ");
+            string input = Console.ReadLine();
+            Console.WriteLine();
+            //Преобразовываем числа в массив
+            string[] parts = input.Split(' ');
+            int[] numbers = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                numbers[i] = int.Parse(parts[i]);
+            }
+            //Проверяем наличие однинаковых элементов
+            bool Duplicates = false;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        Duplicates = true;
+                        break;
+                    }
+                }
+                if (Duplicates) break;
+
+            }
+            //Выводим результат
+            if (Duplicates)
+                Console.WriteLine("В введенном массиве присутствуют одинаковые элементы");
+            else
+                Console.WriteLine("Все элементы массива различнв");
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Lab_1/task_2_lesson_3/task_2_lesson_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2_lesson_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаем массив
+            int[] numbers = new int[4000];
+            Random random = new Random();
+            //Заполняем массив 
+            for (int i = 0; i < 4000; i++)
+            {
+                numbers[i] = random.Next(1, 5001);
+            }
+            //Создаем массив для хранения сумм пар соседних чисел
+            int[] sums = new int[1999];
+            //Вычисляем суммы пар четных чисел
+            for (int i = 0, j = 0; i < 3998; i += 2, j++)
+            {
+                sums[j] = numbers[i] + numbers[i + 1];
+            }
+            //Выводим результат
+            Console.Write("[");
+            for (int i = 0; i < sums.Length; i++)
+            {
+                Console.Write(sums[i]);
+                if (i < sums.Length - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine("]");
+        }
+
+    }
+}
