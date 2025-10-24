2688113b3f67310929aa1ff1b7bbad2454163311
CDeshechka <deniscvetkove@gmail.com>
2025-10-24 21:02:02 +0300

    создание констант перечисления

---
+++ Enum/Enum_TaskStatus/Enum_TaskStatus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENUM
+{
+    public enum TaskStatus
+    {
+        NotStarted,
+        InProgress,
+        Completed,
+        Cancelled
+    }
+
+ 
+}
