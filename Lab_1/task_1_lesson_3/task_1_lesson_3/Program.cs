a83b5f604831bcadc0f00a91563283f51e27fb2e
CDeshechka <deniscvetkove@gmail.com>
2025-10-24 20:39:58 +0300

    Вводим содержимое массива

--- Lab_1/task_1_lesson_3/task_1_lesson_3/Program.cs
+++ Lab_1/task_1_lesson_3/task_1_lesson_3/Program.cs
@@ -14,36 +14,7 @@ namespace task_1_lesson_3
             Console.Write("Введите числа, содержащиеся в массиве через пробел: ");
             string input = Console.ReadLine();
             Console.WriteLine();
-            //Преобразовываем числа в массив
-            string[] parts = input.Split(' ');
-            int[] numbers = new int[parts.Length];
-            for (int i = 0; i < parts.Length; i++)
-            {
-                numbers[i] = int.Parse(parts[i]);
-            }
-            //Проверяем наличие однинаковых элементов
-            bool Duplicates = false;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = i + 1; j < numbers.Length; j++)
-                {
-                    if (numbers[i] == numbers[j])
-                    {
-                        Duplicates = true;
-                        break;
-                    }
-                }
-                if (Duplicates) break;
-
-            }
-            //Выводим результат
-            if (Duplicates)
-                Console.WriteLine("В введенном массиве присутствуют одинаковые элементы");
-            else
-                Console.WriteLine("Все элементы массива различнв");
-
-            Console.WriteLine();
-
+         
         }
     }
 }
